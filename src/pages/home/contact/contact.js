de5e3851756db1d6aae5bc1ba26a1bbecf743e0a
Ateeb Sohail <challengebudies9@gmail.com>
2023-05-05 22:51:24 -0400

    v0.8.4 - Updated Contact

--- src/pages/home/contact/contact.js
+++ src/pages/home/contact/contact.js
@@ -2,19 +2,28 @@ import React from 'react'
 import './contact.css'
 import { FaDiscord, FaWhatsapp, FaInstagram } from 'react-icons/fa'
 function ContactPage() {
+  const whatsApp = () => {
+    window.open('https://wa.me/00923362833222')
+  }
+  const discord = () => {
+    window.open('https://discord.gg/3yHfjcKgr8')
+  }
+  const instagram = () => {
+    window.open('https://instagram.com/grabyourservices')
+  }
   return (
     <div className='contact-header'>
       <div className='contact-container'>
         <h1 className='pricing-header-heading'><span style={{backgroundImage: 'linear-gradient(to right, #7251ff, #c6bcf1)',color: 'transparent', WebkitBackgroundClip: 'text', backgroundClip: 'text'}}>Interested?<br/><span className='pricing-margin'>Get in Touch!</span></span></h1>
         <div className='pricing-container'>
           <div className='contact-item'>
-            <FaWhatsapp className='contact-icon' color="white" size={64}></FaWhatsapp>
+            <FaWhatsapp onClick={whatsApp} className='contact-icon' color="white" size={64}></FaWhatsapp>
           </div>
           <div className='contact-item'>
-            <FaDiscord className='contact-icon' color="#7289da" size={64}></FaDiscord>
+            <FaDiscord onClick={discord} className='contact-icon' color="#7289da" size={64}></FaDiscord>
           </div>
           <div className='contact-item'>
-            <FaInstagram className='contact-icon' color="#d62976" size={64}></FaInstagram>
+            <FaInstagram onClick={instagram} className='contact-icon' color="#d62976" size={64}></FaInstagram>
           </div>
         </div>
       </div>
